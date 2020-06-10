6570c45e9996136a328046455a5b4dd75f672b79
dkwilson <general@ytechnology.net>
2020-06-10 13:23:22 -0400

    added passport local; feature complete

--- app.js
+++ app.js
@@ -1,82 +1,112 @@
 //jshint esversion:6
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const mongoose = require("mongoose")
-const encrypt = require('mongoose-encryption')
-const md5 = require('md5')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
-const userSchema = new mongoose.Schema ({
-    email: String,
-    password: String
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/register", (req, res) => {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
 
-const User = new mongoose.model("User", userSchema)
-
-
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/login', (req, res) => {
-    res.render('login');
-})
-
-app.get('/register', (req, res) => {
-    res.render('register');
-})
-
-app.post('/register', (req, res) => {
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-
-    newUser.save((err) => {
-        if(err){
-            console.log(err);
-        } else {
-            res.render('secrets')
-        }
-    })
-})
-
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = md5(req.body.password);
-
-    User.findOne({email: username}, (err, foundUser) => {
-        if(err) {
-            console.log(err)
-        } else {
-            console.log(foundUser)
-            if (foundUser) {
-                if(foundUser.password === password) {
-                    res.render("secrets")
-                }
-            }
-        }
-    })
-})
-
-
-
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
 
 app.listen(3000, () => {
-    console.log("The server is up and running on port 3000")
-})
+  console.log("The server is up and running on port 3000");
+});
